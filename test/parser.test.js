0bb1e03bef9268920d25c32b766116d2579fcf0a
Christian Kaisermann <christian.kaisermann@stone.com.br>
2019-03-08 13:17:12 -0300

    test: 💍 add basic tests for parser

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,1 +1,27 @@
-describe('parsing', () => {});
+const { parse } = require('../src/parser.js');
+const { AST } = require('../src/ast.js');
+
+const isNumber = n => n != null && Number.isFinite(+n);
+
+const n = (type, left = null, right = null) => {
+  if (isNumber(left)) left = n(left);
+  if (isNumber(right)) right = n(right);
+
+  /** operators nodes */
+  if (!isNumber(type)) return { type, left, right };
+
+  return { type: 'number', value: `${type}`, left, right };
+};
+
+describe('parsing', () => {
+  it('should create an AST from tokens', () => {
+    expect(parse('2 + 2').get()).toMatchObject(n('+', 2, 2));
+    expect(parse('2 + 10 / 2').get()).toMatchObject(n('+', 2, n('/', 10, 2)));
+  });
+
+  it('should process same precedence operators correctly', () => {
+    expect(parse('7 - 6 / 2 * 4').get()).toMatchObject(
+      n('-', 7, n('*', n('/', 6, 2), 4)),
+    );
+  });
+});
